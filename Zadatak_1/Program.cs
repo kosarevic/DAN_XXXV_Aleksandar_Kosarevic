4ac1a5294eca7be9f3406c95db8b20617d6c33a7
Kosarevic <kosarevic@gmail.com>
2020-06-19 11:49:29 +0200

    Created Generator() method

--- Zadatak_1/Program.cs
+++ Zadatak_1/Program.cs
@@ -11,14 +11,13 @@ namespace Zadatak_1
     {
         public static int users = 0;
         public static int number = 0;
+        public static List<Thread> threads = new List<Thread>();
 
         static void Main(string[] args)
         {
             Thread t = new Thread(Menu);
             t.Start();
             t.Join();
-
-            Thread Thread_Generator = new Thread(Generator);
 
         }
 
@@ -28,9 +27,26 @@ namespace Zadatak_1
             users = int.Parse(Console.ReadLine());
             Console.WriteLine("Input number to be guessed. (1-100)");
             number = int.Parse(Console.ReadLine());
+
+            Thread Thread_Generator = new Thread(Generator);
+            Thread_Generator.Start();
+
+            Console.WriteLine("User successfully inserted participants.");
+            Console.WriteLine("Number of participants is: {0}", users);
+            Console.WriteLine("Chosen number to be guessed: {0}", number);
         }
 
         static void Generator()
+        {
+            for (int i = 1; i <= users; i++)
+            {
+                Thread t = new Thread();
+                t.Name = string.Format("Participant_{0}", i);
+                threads.Add(t);
+            }
+        }
+
+        static void AddToList()
         {
 
         }

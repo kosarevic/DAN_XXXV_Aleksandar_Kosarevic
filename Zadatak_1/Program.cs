4e416d99d2b29fa1d79bb1c73a18040b5e5ca91b
Kosarevic <kosarevic@gmail.com>
2020-06-19 11:36:55 +0200

    Created Menu() method

--- Zadatak_1/Program.cs
+++ Zadatak_1/Program.cs
@@ -2,15 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
     class Program
     {
+        public static int users = 0;
+        public static int number = 0;
+
         static void Main(string[] args)
         {
-            
+            Thread t = new Thread(Menu);
+            t.Start();
+            t.Join();
+
+            Thread Thread_Generator = new Thread(Generator);
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Input how many users will participate in a game.");
+            users = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input number to be guessed. (1-100)");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        static void Generator()
+        {
+
         }
     }
 }
